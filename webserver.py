4d4c45b1bae285976260ee310ce9bf7ee60154f1
96kevrud <kevinleerudnick@gmail.com>
2021-03-20 16:40:58 +0100

    Implement post handeling  and update webpage

--- webserver.py
+++ webserver.py
@@ -2,38 +2,33 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 import io
 import urllib
+from temp_sensor import Sensor
+from local_switch import LocalSwitch
+
 
 class ControlServer(BaseHTTPRequestHandler):
+    sensor = None
+    fridge = None
     temp = 0
+    def get_response_body(self):
+        print(self.sensor.temp())
+        with io.open("webpage.html","r", encoding="utf-8") as f:
+            text = f.read()
+            text = text.replace("%TAR_TEMP%", str(self.temp))
+            text = text.replace("%CUR_TEMP%", str(self.sensor.temp()))
+            text = text.replace("%ON_OFF_FRIDGE%", self.fridge.on_off_str())
+        return bytes(text, "utf-8")
+    
     def do_POST(self):
-        print("POST POST POST")
         length = int(self.headers['Content-Length'])
         post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
-        print(int(post_data["temp"][0]))
-        self.temp = int(post_data["temp"][0])
+        if("temp" in post_data):
+            self.temp = int(post_data["temp"][0])
         self.do_GET()
         
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        with io.open("webpage.html","r", encoding="utf-8") as f:
-            text = f.read()
-            text = text.replace("%TEMP%", str(self.temp))
-            
-        self.wfile.write(bytes(text, "utf-8"))
-
-
-if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = 8765
-    webServer = HTTPServer((host, port), ControlServer)
-    print("Server started http://%s:%s" % (host, port))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
+        body = self.get_response_body()
+        self.wfile.write(body)    
